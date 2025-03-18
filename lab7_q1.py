ad84be94558a974a6620217805f49c13c8c0c789
Tenzin Sommer <119877329+TenzinSommer@users.noreply.github.com>
2025-03-18 19:28:55 -0400

    halfway-done pt 1

---
+++ lab7_q1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from scipy.stats import poisson
+
+df = pd.read_csv('traffic_data_f1.csv')
+
+def get_congestion_prob(df):
+
+    vehicle_count = df.groupby('Weekday').count()
+    
+    vehicleCountDF = vehicle_count[['Weight_kg']]
+    vehicleCountDF = vehicleCountDF.rename(columns={"Weight_kg": "Count"})
+
+
+    return vehicleCountDF['Count'] / (df.groupby('Weekday')['Second'].mean() / 60)
+
+print(get_congestion_prob(df))
